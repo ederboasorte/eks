08dca500b1d670a70c1b18be5b9851a49a127f3e
Eder Vilela Brandão <ederboasorte@hotmail.com>
2026-01-28 22:02:16 -0300

    Commit

---
+++ networking/variables.tf
@@ -0,0 +1,33 @@
+variable "project_name" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "CIDR principal da VPC"
+}
+
+variable "vpc_additional_cidrs" {
+  type        = list(string)
+  description = "Lista de CIDR's adicionais da VPC"
+  default     = []
+}
+
+resource "aws_vpc_ipv4_cidr_block_association" "main" {
+  count      = length(var.vpc_additional_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.vpc_additional_cidrs[count.index]
+}
+
+variable "public_subnets" {
+  description = "Lista de Public Subnets da VPC"
+  type = list(object({
+    name              = string
+    cidr              = string
+    availability_zone = string
+  }))
+}
